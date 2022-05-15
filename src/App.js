9ba71a6fb57560a50d48423a8f5cbbe43f41ec26
Jewell Scott <jewelljrscott@gmail.com>
2022-05-15 04:15:57 -0400

    add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header'
+import Footer from './components/Footer';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
+import About from './components/About';
 
 function App() {
 
@@ -90,11 +93,26 @@ const toggleReminder = async (id) => {
     : task))
 }
   return (
+    <Router>
     <div className="container">
       <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-      {showAddTask && <AddTask onAdd={addTask}/>}
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'There are no tasks. Click "Add" to add more!'}
+      <Routes>
+        <Route 
+        path='/' 
+        element={
+          <>
+          {showAddTask && <AddTask onAdd={addTask}/>}
+          {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'There are no tasks. Click "Add" to add more!'}
+          </>
+        }
+      />      
+      <Route path='/about' element={<About/>}/>
+      </Routes>
+      
+      <Footer/>
+    
     </div>
+    </Router>
   );
 }
 
